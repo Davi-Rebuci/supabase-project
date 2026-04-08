9a43158282fe1c52e6cd2a1b9a1186231c2fca54
Davi Rebuci Rodrigues <0000110803150xsp@aluno.educacao.sp.gov.br>
2026-04-08 16:15:07 -0300

    feat: dashboard real-time com >> supabase

---
+++ app.js
@@ -0,0 +1,54 @@
+// 1. CONFIGURAÇÃO (Substitua pelos dados do SEU painel do Supabase)
+const SUPABASE_URL = 'https://kepatjcoinnhtadxnurd.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlcGF0amNvaW5uaHRhZHhudXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NjU4NDEsImV4cCI6MjA5MTI0MTg0MX0.NqgA31UTERgmp7akxcmKBwRWdrMSfQlje_XUNvwQSZA';
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Referências do HTML
+const listaHTML = document.getElementById('listaDeAlertas');
+// --- FUNÇÃO 1: ENVIAR (Simulando API POST) ---
+async function enviarParaBanco() {
+const msg = document.getElementById('campoMensagem').value;
+const nivel = document.getElementById('campoNivel').value;
+if (!msg) return alert("Escreva algo!");
+// Grava no banco de dados
+const { error } = await supabaseClient
+.from('alertas')
+.insert([{ mensagem: msg, nivel: nivel }]);
+if (error) console.error("Erro ao enviar:", error.message);
+document.getElementById('campoMensagem').value = ""; // Limpa campo
+}
+// --- FUNÇÃO 2: ESCUTAR (WebSocket / Real-time) ---
+function ligarMonitoramento() {
+    supabaseClient
+    .channel('public:alertas') // Escuta o canal da tabela alertas
+    .on('postgres_changes', { event: 'INSERT', schema: 'public', table:
+    'alertas' }, (payload) => {
+    // Quando um novo dado chegar, renderiza na tela
+    mostrarNaTela(payload.new);
+    })
+    .subscribe();
+    }
+    // --- FUNÇÃO 3: RENDERIZAR NA TELA ---
+    function mostrarNaTela(alerta) {
+    const item = document.createElement('div');
+    // Cores baseadas no nível
+    let corDestaque = "border-primary";
+    
+    if(alerta.nivel === 'critico') corDestaque = "border-danger bg-danger bg-opacity-10";
+    if(alerta.nivel === 'aviso') corDestaque = "border-warning bg-warning bg-opacity-10";
+    
+    item.className = `list-group-item bg-dark text-white mb-2 shadow-sm
+    border-start border-4 ${corDestaque} animate__animated animate__fadeInLeft`;
+    item.innerHTML = `
+    <div class="d-flex justify-content-between align-items-center">
+    <div>
+    <strong class="text-uppercase small">${alerta.nivel}</strong>
+    <p class="mb-0">${alerta.mensagem}</p>
+    </div>
+    <small class="text-muted">${new
+    Date(alerta.criado_em).toLocaleTimeString()}</small>
+    </div>
+    `;
+    listaHTML.prepend(item); // Novo alerta sempre no topo
+    }
+    // Inicializa o WebSocket e carrega dados existentes
+    ligarMonitoramento();
